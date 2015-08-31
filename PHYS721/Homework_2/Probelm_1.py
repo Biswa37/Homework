ee745f912ce818ce6a5cb51186e7020fc1626c61
Nick Tyler <tylern@canisius.edu>
2015-08-31 17:37:59 -0400

    Cleanup and tex file

--- PHYS721/Homework_2/Probelm_1.py
+++ PHYS721/Homework_2/Probelm_1.py
@@ -20,8 +20,6 @@ vec1 = TLorentzVector()
 vec2 = TLorentzVector()
 lines = [line.rstrip('\n') for line in open('data1')]
 
-EnergyHist1 = TH1F('EnergyHist1','Energy',25,0,2.5)
-EnergyHist2 = TH1F('EnergyHist2','Energy',25,0,2.5)
 fig = plt.figure(num=None, figsize=(8, 8), dpi=800, facecolor='w', edgecolor='k')
 
 for line in lines:
@@ -29,9 +27,7 @@ for line in lines:
     vec1.SetPxPyPzE(a[1],a[2],a[3],a[0])
     vec2.SetPxPyPzE(a[5],a[6],a[7],a[4])
     energy1.append(vec1.E())
-    EnergyHist1.Fill(vec1.E())
     energy2.append(vec2.E())
-    EnergyHist2.Fill(vec2.E())
 
 plt.hist(energy1, 25, histtype=u'stepfilled' , alpha=0.5)
 plt.xlabel(r'Energy (GeV)')
--- PHYS721/Homework_2/Probelm_2.py
+++ PHYS721/Homework_2/Probelm_2.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import ROOT
-
+font = {'size'	:	10}
 #####Homework 2 Question 2
 mass1 = []
 mass2 = []
@@ -18,38 +18,36 @@ mass_sum = []
 vec1 = TLorentzVector()
 vec2 = TLorentzVector()
 lines = [line.rstrip('\n') for line in open('data1')]
-bins = 50
-MassHist1 = TH1F('MassHist1','Mass 1',bins,0.488,0.5)
-MassHist2 = TH1F('MassHist2','Mass 2',bins,0.48,0.5)
-fig = plt.figure(num=None, figsize=(20, 20), dpi=800, facecolor='w', edgecolor='k')
+bins = 75
+
+fig = plt.figure(num=None, figsize=(10, 10), dpi=200, facecolor='w', edgecolor='k')
+matplotlib.rc('font', **font)
 
 for line in lines:
     a = np.array(np.loadtxt(StringIO(line)))
     vec1.SetPxPyPzE(a[1],a[2],a[3],a[0])
     vec2.SetPxPyPzE(a[5],a[6],a[7],a[4])
     mass1.append(vec1.M())
-    MassHist1.Fill(vec1.M())
     mass2.append(vec2.M())
-    MassHist2.Fill(vec2.M())
     mass_sum.append((vec1+vec2).M())
 
 plt.subplot(221)
 plt.hist(mass1, bins, histtype=u'stepfilled', alpha=0.5)
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
-plt.title(r'Mass four vector 1')
+plt.title(r'Mass found from four vector 1')
 
 plt.subplot(222)
 plt.hist(mass2, bins, histtype=u'stepfilled',facecolor='red' , alpha=0.5)
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
-plt.title(r'Mass four vector 2')
+plt.title(r'Mass found from four vector 2')
 
 plt.subplot(212)
-plt.hist(mass_sum, 2*bins, histtype=u'stepfilled',facecolor='green' , alpha=0.5)
+plt.hist(mass_sum, 4*bins, histtype=u'stepfilled',facecolor='green' , alpha=0.5)
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
-plt.title(r'Sum of Masses')
+plt.title(r'Mass found from the sum of four vectors')
 #plt.show()
 
 fig.savefig('Problem_2.pdf')
