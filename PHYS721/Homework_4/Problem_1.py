f76eb2a7c81b16a6bf64980c081f90374cf196cf
Nick Tyler <tylern@canisius.edu>
2015-09-14 19:07:54 -0400

    Finished Kinda

--- PHYS721/Homework_4/Problem_1.py
+++ PHYS721/Homework_4/Problem_1.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 import numpy 
 import numpy as np
-from StringIO import StringIO
-from ROOT import TLorentzVector
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+from scipy.integrate import quad
 
 fig = plt.figure(num=None, figsize=(15, 15), dpi=200, facecolor='w', edgecolor='k')
-lines = [line.rstrip('\n') for line in open('data1')]
-mass_sum = []
-vec1 = TLorentzVector()
-vec2 = TLorentzVector()
-num_bins = 60
-
-for line in lines:
-    a = np.array(np.loadtxt(StringIO(line)))
-    vec1.SetPxPyPzE(a[1],a[2],a[3],a[0])
-    vec2.SetPxPyPzE(a[5],a[6],a[7],a[4])
-    mass_sum.append((vec1+vec2).M())
+num_bins = 1000
 
 def myBW(Energy,Mass,Gamma):
     g = ((Mass**2.0 + Gamma**2.0)*Mass**2.0)**(1.0/2.0)
@@ -26,43 +15,37 @@ def myBW(Energy,Mass,Gamma):
 
 def BW_NonR(Energy,Mass,Gamma):
     return (((Gamma/(2.0*np.pi)))/((Energy-Mass)**2.0 + (Gamma/2.0)**2.0))
+ 
+def myBW_2(Energy,Mass,Gamma_0):
+    g = ((Mass**2.0 + Gamma_P(Energy,Gamma_0)**2.0)*Mass**2.0)**(1.0/2.0)
+    k = (2.0 * 2.0**(1.0/2.0) * Mass * Gamma_P(Energy,Gamma_0) * g)/(np.pi * (Mass**(2.0)+g)**(1.0/2.0))
+    return (k/((Energy**2.0-Mass**2.0)**2.0 + (Gamma_P(Energy,Gamma_0)*Mass)**2.0))
 
-"""
-So I think the idea is that you have gamma(p) = avg(gamma)*(p/p0)**3.0
-To get P we can use the energy E^2 = p^2 + m^2 or p = sqrt(E^2-m^2)
-m = avg(mass)
-p0 = mass???
-"""
-#This is not right I still need to fix this  
-def myBW_2(Energy,Mass):
-    g = ((Mass**2.0 + Gamma_P(Energy)**2.0)*Mass**2.0)**(1.0/2.0)
-    k = (2.0 * 2.0**(1.0/2.0) * Mass * Gamma_P(Energy) * g)/(np.pi * (Mass**(2.0)+g)**(1.0/2.0))
-    return (k/((Energy**2.0-Mass**2.0)**2.0 + (Gamma_P(Energy)*Mass)**2.0))
+def Gamma_P(Energy,Gamma_0):
+    return Gamma_0#*(p/p0)**3.0
 
-def Gamma_P(Energy):
-    return 0.5*(popt_1[1]+popt_2[1])#*(p/p0)^3.0
-#Up to here
+def FWHM(x,y):
+    spline = UnivariateSpline(x,y)
+    return spline.roots()/2.0
 
-hist, bin_edges = numpy.histogram(mass_sum,bins=num_bins)
-xdata = 0.5*(bin_edges[1:]+bin_edges[:-1])
-ydata = hist
-n, bins, patches = plt.hist(mass_sum, num_bins, 
-    histtype=u'stepfilled',facecolor='green' , alpha=0.5,normed=True)
-x0 = numpy.array([1.02, 0.0043])
-    
-popt_1, pcov_1 = curve_fit(myBW, xdata, ydata, p0=x0)
-plt.plot(xdata,myBW(xdata,popt_1[0],popt_1[1]),'g-',lw=4,
-    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.7f \ GeV,}\ \Gamma=%.7f$' %(popt_1[0], popt_1[1]))
-    
-popt_2, pcov_2 = curve_fit(BW_NonR, xdata, ydata, p0=x0)
-plt.plot(xdata,BW_NonR(xdata,popt_2[0],popt_2[1]),'r--',lw=2,
-    label=r'$\mathrm{Non-Rel. \ BW:\ Mass=%.7f \ GeV,}\ \Gamma=%.7f$' %(popt_2[0], popt_2[1]))
+x = np.linspace(0.99, 1.08, num_bins)
+y1 = myBW(x,1.019461,0.00426)
+y2 = BW_NonR(x,1.019461,0.00426)
+y3 = myBW_2(x,1.019461,0.00426)
 
-#This part too
-popt_3, pcov_3 = curve_fit(myBW_2, xdata, ydata, p0=x0[0])
-plt.plot(xdata,myBW_2(xdata,popt_3[0]),'b-.',lw=2,
-    label=r'$\mathrm{BW:\ Mass=%.7f \ GeV,}\ \Gamma=%.7f$' %(popt_3[0], Gamma_P(popt_3[0])))
-    
+plt.plot(x,y1,'g-',lw=2,
+    label=r'$\mathrm{Relatavistic \ BW:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y1), FWHM(x,y1)))
+
+plt.plot(x,y2,'r--',lw=2,
+    label=r'$\mathrm{Non-Rel. \ BW:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y2), FWHM(x,y2)))
+
+plt.plot(x,y3,'b-.',lw=2,
+    label=r'$\mathrm{Relatavistic \ BW:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y3), FWHM(x,y3)))
+
+print quad(myBW, 0, 200, args=(1.019461,0.00426))[0] 
+print quad(myBW_2, 0, 200, args=(1.019461,0.00426))[0] 
+print quad(BW_NonR, 0, 200, args=(1.019461,0.00426))[0]
+
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
 plt.legend()
