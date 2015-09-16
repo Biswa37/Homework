82c2f87ac11b379ae4f6f8d29634e4bad6deb9e0
Nick Tyler <tylern@canisius.edu>
2015-09-16 10:37:14 -0400

    Yeah I'm done I don't care

--- PHYS721/Homework_4/Problem_1.py
+++ PHYS721/Homework_4/Problem_1.py
@@ -2,15 +2,16 @@
 import numpy 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import UnivariateSpline
 from scipy.integrate import quad
-from scipy.optimize import curve_fit
-import sys
 
 fig = plt.figure(num=None, figsize=(15, 15), dpi=200, facecolor='w', edgecolor='k')
-num_bins = 10000000
+num_bins = 10000
+m_k = 0.493677
+m_phi = 1.019461
+mass = 1.019461
+gamma = 0.00426
 
-def myBW(Energy,Mass,Gamma):
+def BW(Energy,Mass,Gamma):
     g = ((Mass**2.0 + Gamma**2.0)*Mass**2.0)**(1.0/2.0)
     k = (2.0 * 2.0**(1.0/2.0) * Mass * Gamma * g)/(np.pi * (Mass**(2.0)+g)**(1.0/2.0))
     return (k/((Energy**2.0-Mass**2.0)**2.0 + (Gamma*Mass)**2.0))
@@ -18,17 +19,17 @@ def myBW(Energy,Mass,Gamma):
 def BW_NonR(Energy,Mass,Gamma):
     return (((Gamma/(2.0*np.pi)))/((Energy-Mass)**2.0 + (Gamma/2.0)**2.0))
  
-def myBW_2(Energy,Mass,Gamma_0):
+def BW_2(Energy,Mass,Gamma_0):
     g = ((Mass**2.0 + Gamma_P(Energy,Gamma_0)**2.0)*Mass**2.0)**(1.0/2.0)
     k = (2.0 * 2.0**(1.0/2.0) * Mass * Gamma_P(Energy,Gamma_0) * g)/(np.pi * (Mass**(2.0)+g)**(1.0/2.0))
     return (k/((Energy**2.0-Mass**2.0)**2.0 + (Gamma_P(Energy,Gamma_0)*Mass)**2.0))
 
+
 def Gamma_P(Energy,Gamma_0):
-    m_k = 0.493677
-    m_phi = 1.019461
-    p = abs((Energy**2.0/4.0)-m_k**2.0)**(1.0/2.0)
+    p = ((Energy**2.0/4.0)-m_k**2.0)**(1.0/2.0)
     p0 = ((m_phi**2.0/4.0)-m_k**2.0)**(1.0/2.0)
     return Gamma_0*(p/p0)**3.0
+
 
 """
 My homemade algorithm for getting FWHM
@@ -38,40 +39,40 @@ Imporve it by chaning sigma to be dynamic to adapt to changing widths
 def FWHM(x,y):
     temp = []
     max_2 = y.max()/2.0
-    sigma = 10 #this needs to be dynamic so that it only gets two values
-    #YMaxIndex = numpy.where(y == y.max())[0][0]
+    sigma = 0.25 #this should be dynamic so that it only gets two values
 
     for value in y:
         if value >= max_2-sigma and value <= max_2+sigma:
             temp.append(numpy.where(y == value)[0][0])
 
-    temp = x[temp]
+    temp = x[temp] #convert bin numbers to x values
     return temp, (temp[-1] - temp[0])
 
 def normalize(values):
-    return values/num_bins
+    return values
 
 x = np.linspace(0.99, 1.1, num_bins)
-y1 = myBW(x,1.019461,0.00426)
-y2 = BW_NonR(x,1.019461,0.00426)
-y3 = myBW_2(x,1.019461,0.00426)
+y1 = BW(x,mass,gamma)
+y2 = BW_NonR(x,mass,gamma)
+y3 = BW_2(x,mass,gamma)
 
-temp, val_1 = FWHM(x,y1)
-temp, val_2 = FWHM(x,y2)
-temp, val_3 = FWHM(x,y3)
+temp_1, val_1 = FWHM(x,y1)
+temp_2, val_2 = FWHM(x,y2)
+temp_3, val_3 = FWHM(x,y3)
 
 
-plt.plot(x,normalize(y1),'g-',lw=4,
+plt.plot(x,y1,'g-',lw=4,
     label=r'$\mathrm{Relatavistic \ BW:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y1), val_1))
 
-plt.plot(x,normalize(y2),'r--',lw=4,
+plt.plot(x,y2,'r--',lw=4,
     label=r'$\mathrm{Non-Rel. \ BW:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y2), val_2))
 
 
-plt.plot(x,normalize(y3),'b-.',lw=6,
+plt.plot(x,y3,'b-.',lw=6,
     label=r'$\mathrm{Rel. \ BW \ with \ Mass \ dep. \ \Gamma:\ Peak=%.7f \ GeV,}\ FWHM=%.7f$' %(max(y3), val_3))
 
 """
+These are visual checks of the FWHM. The more horiontal the line the better accuacy of FWHM.
 plt.plot(
     [ temp[0] , temp[-1]  ],
     [ normalize(y1)[numpy.where(x == temp[0])[0][0]], normalize(y1)[numpy.where(x == temp[-1])[0][0]] ],
@@ -90,6 +91,7 @@ plt.plot(
     'b', lw=2
     )
 """
+
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
 plt.legend()
