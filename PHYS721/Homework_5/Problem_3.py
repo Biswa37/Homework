4bda864db74bf453e5c570601deabd915e88667c
Nick Tyler <tylern@canisius.edu>
2015-09-17 14:18:30 -0400

    Done with problems just do tex

--- PHYS721/Homework_5/Problem_3.py
+++ PHYS721/Homework_5/Problem_3.py
@@ -6,6 +6,8 @@ from scipy.integrate import quad
 from StringIO import StringIO
 from ROOT import TLorentzVector
 from scipy.optimize import curve_fit
+from ROOT import TRandom
+from ROOT import TF1
 
 fig = plt.figure(num=None, figsize=(15, 15), dpi=200, facecolor='w', edgecolor='k')
 m_k = 0.493677
@@ -34,14 +36,6 @@ def P_fac(Energy):
     p0 = ((m_phi**2.0/4.0)-m_k**2.0)**(1.0/2.0)
     return (p/p0)**3.0
 
-#change coeficiets to tune graph
-def bac_poly(x):
-    c1 = 1
-    c2 = 1
-    c3 = 1
-    return c1 * x**2.0 + c2 * x + c3
-
-
 lines = [line.rstrip('\n') for line in open('data1')]
 
 for line in lines:
@@ -51,35 +45,52 @@ for line in lines:
     mass_sum.append((vec1+vec2).M())
 
 #new stuff###
-bkg = numpy.random.uniform(0.99, 1.09, size=len(mass_sum))
-bkg = bac_poly(bkg)
+f1 = TF1("f1","[0]*x*x+[1]*x+[2]",0.99,1.09)
+""" Concave
+f1.SetParameter(0,66000.0)
+f1.SetParameter(1,-137780.0)
+f1.SetParameter(2,71916.0)
+"""
+""" Convex
+f1.SetParameter(0,-36000.0)
+f1.SetParameter(1,73080.0)
+f1.SetParameter(2,-36866.0)
+"""
+"""reasonable """
+f1.SetParameter(0,-3.0)
+f1.SetParameter(1,7.0)
+f1.SetParameter(2,-3.0)
 
+r = [] 
+for i in range(len(mass_sum)):
+    r.append(f1.GetRandom())
+
+bkg = np.array(r)
+mass_sum = np.concatenate([mass_sum, bkg])
 ########
 
 hist, bin_edges = numpy.histogram(mass_sum,bins=num_bins)
-###
-bkg_hist, bkg_bin_edges = numpy.histogram(bkg,bins=num_bins)
 xdata = 0.5*(bin_edges[1:]+bin_edges[:-1])
 ydata = hist
 
-n, bins, patches = plt.hist(mass_sum, num_bins, histtype=u'stepfilled',facecolor='b' , alpha=0.5)
+n, bins, patches = plt.hist(mass_sum, num_bins, histtype=u'stepfilled',facecolor='g' , alpha=0.5)
 
 x0 = np.array([1.02,0.0043])
 
 popt_3, pcov_3 = curve_fit(BW_2, xdata, ydata, p0=x0)
 perr_3 = np.sqrt(np.diag(pcov_3))
 plt.plot(xdata,BW_2(xdata,popt_3[0],popt_3[1]),'g-', lw=4,
-    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.7f \pm %.7f \ GeV,}\ \Gamma=%.7f \pm %.7f$' %(popt_3[0], perr_3[0], popt_3[1], perr_3[1]))
+    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.6f \pm %.6f \ GeV,}\ \Gamma=%.6f \pm %.6f$' %(popt_3[0], perr_3[0], popt_3[1], perr_3[1]))
 
 popt_1, pcov_1 = curve_fit(BW, xdata, ydata, p0=x0)
 perr_1 = np.sqrt(np.diag(pcov_1))
 plt.plot(xdata,BW(xdata,popt_1[0],popt_1[1]),'b-.', lw=4,
-    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.7f \pm %.7f \ GeV,}\ \Gamma=%.7f \pm %.7f$' %(popt_1[0], perr_1[0], popt_1[1], perr_1[1]))
+    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.6f \pm %.6f \ GeV,}\ \Gamma=%.6f \pm %.6f$' %(popt_1[0], perr_1[0], popt_1[1], perr_1[1]))
 
 popt_2, pcov_2 = curve_fit(BW_NonR, xdata, ydata, p0=x0)
 perr_2 = np.sqrt(np.diag(pcov_2))
 plt.plot(xdata,BW_NonR(xdata,popt_2[0],popt_2[1]),'r--', lw=4,
-    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.7f \pm %.7f \ GeV,}\ \Gamma=%.7f \pm %.7f$' %(popt_2[0], perr_2[0], popt_2[1], perr_2[1]))
+    label=r'$\mathrm{Relatavistic \ BW:\ Mass=%.6f \pm %.6f \ GeV,}\ \Gamma=%.6f \pm %.6f$' %(popt_2[0], perr_2[0], popt_2[1], perr_2[1]))
 
 plt.xlabel(r'Mass (GeV)')
 plt.ylabel(r'Counts (#)')
